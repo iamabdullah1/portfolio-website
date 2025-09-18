2542100300e036c97e1fa347b34f193d972f3b12
iamabdullah1 <iamabdullahakram1@gmail.com>
2025-09-18 09:51:55 -0700

    Import Globe component directly instead of using lazy loading

--- portfolio frontend/src/sections/About.jsx
+++ portfolio frontend/src/sections/About.jsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect, useState, useCallback, lazy } from "react";
 import Card from "../components/Card";
-import TechCards from "../components/TechCards";
-const Globe = lazy(() => import('../components/Globe'));
+import TechCards from "../components/TechCards"; 
+import Globe from '../components/Globe';
 import Matter from 'matter-js';
 // import Contact3D from "../components/Contact3D";
 
